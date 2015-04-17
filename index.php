0a688735d509ed8fa8c35028342139eb37c73f8a
Juan <jhuhandha@gmail.com>
2015-04-17 09:09:45 -0500

    Cambio ruta mongo

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
-
-	$conexion = new MongoClient( "mongodb://root:root@ds061601.mongolab.com:61601" ); // concectar a un host remoto en un puerto dado
+	
+	//"mongodb://root:root@ds061601.mongolab.com:61601"
+	$conexion = new MongoClient(  ); // concectar a un host remoto en un puerto dado
 	var_dump($conexion);
  ?>
