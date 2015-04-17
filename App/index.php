ca8492022d92a51f289cedffe819d77d9bcc9785
Juan <jhuhandha@gmail.com>
2015-04-17 09:38:42 -0500

    Subo mongo

--- App/index.php
+++ App/index.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	//"mongodb://root:root@ds061601.mongolab.com:61601"
-	// $conexion = new MongoClient(  ); // concectar a un host remoto en un puerto dado
-	// var_dump($conexion);
-	echo "Hola";
+	$conexion = new MongoClient(  ); // concectar a un host remoto en un puerto dado
+	var_dump($conexion);
+	
  ?>
